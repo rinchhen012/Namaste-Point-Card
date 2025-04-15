4f30594fa8dfac8c00f095dba775b1b62dc55df4
yk11-mbp <rinchhen.meh@gmail.com>
2025-04-15 17:09:49 +0900

    Update icon in InfoPage component for improved visual representation

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -143,7 +143,7 @@ const InfoPage: React.FC = () => {
                 </p>
                  <p className="flex items-start">
                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-500 mr-2 mt-0.5 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899l4-4a4 4 0 005.656 5.656l-1.1 1.1" />
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                    </svg>
                    <a href={storeInfo.website} target="_blank" rel="noopener noreferrer" className="text-primary hover:underline break-all">
                      {storeInfo.website}
